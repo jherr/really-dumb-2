925f243ce60c9afd36af7e42743a5b826b339c23
Jack Herrington <jherr@pobox.com>
2024-06-06 20:06:56 -0700

    Count 10

--- committer.sh
+++ committer.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-# Loop from 1 to 10000
-for ((i=1; i<=10000; i++))
+for ((i=10; i<=10000; i++))
 do
-  # Print the current number
   echo $i > count.txt
   git add -A
   git commit -m "Count $i"
   git push -u origin main
-  # Sleep for 10 seconds
-  sleep 10
+  sleep 60
 done
